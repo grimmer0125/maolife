8f2cdd605383c08197746f7a74e37d9b8d4771c7
Grimmer Kang <grimmer0125@gmail.com>
2017-04-19 01:29:08 +0800

    1. add "npm run ios", "npm run android" 2. add "react-native-vector-icons" and run "react-native link" for it. 3. use github version of react-navigation. 4. Practice and add Tab layout by using TabNavigator of react-navigation.

--- js/App.js
+++ js/App.js
@@ -6,20 +6,26 @@ import {
   Alert,
   Button,
   Text,
-  View
+  View,
 } from 'react-native';
+
+import {
+  StackNavigator,
+  TabNavigator,
+} from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const onButtonPress = () => {
   Alert.alert('Button has been pressed!');
 };
 
-export default class App extends Component {
+class MyHomeScreen extends Component {
 
   // Initialize the hardcoded data
   constructor(props) {
     super(props);
     // test();
-    console.log("abc");
+    // console.log("abc");
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       dataSource: ds.cloneWithRows([
@@ -51,42 +57,115 @@ export default class App extends Component {
       </View>
     );
   }
-
-
-  // render() {
-  //   console.log("grimmer test");
-  //   return (
-  //     <View style={styles.container}>
-  //       <Text style={styles.welcome}>
-  //         Welcome to React Native!
-  //       </Text>
-  //       <Text style={styles.instructions}>
-  //         To get started, edit index.ios.js
-  //       </Text>
-  //       <Text style={styles.instructions}>
-  //         Press Cmd+R to reload,{'\n'}
-  //         Cmd+D or shake for dev menu
-  //       </Text>
-  //     </View>
-  //   );
-  // }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+
+class MyNotificationsScreen extends React.Component {
+  // static navigationOptions = {
+  //   tabBarLabel: 'Notifications',
+  //   tabBarIcon: ({ tintColor }) => (
+  //     <Image
+  //       source={require('./notif-icon.png')}
+  //       style={[styles.icon, {tintColor: tintColor}]}
+  //     />
+  //   ),
+  // };
+
+  render() {
+    return (
+      <Button
+        onPress={() => this.props.navigation.goBack()}
+        title="Go back home"
+      />
+    );
+  }
+}
+
+const App = TabNavigator({
+  Home: {
+    screen: MyHomeScreen,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Ionicons
+        name={focused ? 'ios-home' : 'ios-home-outline'}
+        size={26}
+        style={{ color: tintColor }}
+        />
+      ),
+    },
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  Notifications: {
+    screen: MyNotificationsScreen,
+    navigationOptions: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Ionicons
+          name={focused ? 'ios-settings' : 'ios-settings-outline'}
+          size={26}
+          style={{ color: tintColor }}
+        />
+      ),
+    },
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+}, {
+  tabBarOptions: {
+    activeTintColor: '#e91e63',
   },
 });
+
+export default App;
+
+
+// const StacksInTabs = TabNavigator({
+//   MainTab: {
+//     screen: MainTab,
+//     path: '/',
+//     navigationOptions: {
+//       tabBarLabel: 'Home',
+//       tabBarIcon: ({ tintColor, focused }) => (
+//         <Ionicons
+//           name={focused ? 'ios-home' : 'ios-home-outline'}
+//           size={26}
+//           style={{ color: tintColor }}
+//         />
+//       ),
+//     },
+//   },
+//   SettingsTab: {
+//     screen: SettingsTab,
+//     path: '/settings',
+//     navigationOptions: {
+//       tabBarLabel: 'Settings',
+//       tabBarIcon: ({ tintColor, focused }) => (
+//         <Ionicons
+//           name={focused ? 'ios-settings' : 'ios-settings-outline'}
+//           size={26}
+//           style={{ color: tintColor }}
+//         />
+//       ),
+//     },
+//   },
+// }, {
+//   tabBarPosition: 'bottom',
+//   animationEnabled: false,
+//   swipeEnabled: false,
+// });
