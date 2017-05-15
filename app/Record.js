d960dce8656dbaa28a3bdf2e981edea7df4274b9
Grimmer <grimmer0125@gmail.com>
2017-05-16 00:41:01 +0800

    add the countdown UI.

--- app/Record.js
+++ app/Record.js
@@ -18,16 +18,32 @@ import { Container,
   Text,
   ListItem, Radio } from 'native-base';
 
+  import {
+    // Text,
+    // TextInput,
+    View,
+    // Button,
+  } from 'react-native';
+
 import { connect } from 'react-redux';
 
 import {
   StackNavigator,
 } from 'react-navigation';
 
-import Enum from "es6-enum";
+// import Enum from "es6-enum";
 
 const BUTTON_STATUS_INIT = "Click to Start";
 const BUTTON_STATUS_RUNNING = "Click to Cancel";
+const BUTTON_STATUS_END = "End";
+
+const TOTAL_SECONDS = 5;
+
+const initialState = {
+  buttonStatus: BUTTON_STATUS_INIT,
+  seconds: TOTAL_SECONDS,
+  numberOfBreath: null,
+};
 
 class Record extends React.Component {
   static navigationOptions = ({ navigation }) => ({
@@ -35,32 +51,88 @@ class Record extends React.Component {
   });
 
   constructor(props) {
-   super(props);
+    super(props);
 
-   this.state = {
+    console.log("record init");
+    this.state = {
       sleepRadio: true,
       // radio2: false,
       // radio3: false,
       // radio4: true,
-      buttonStatus: BUTTON_STATUS_INIT,
+      // numberOfBreath: -1,
     };
+    this.state = {...initialState};
 
+    // this.resetSeconds();
+
+    this.timerID = -1;
+  }
+
+  resetSeconds() {
+
+    console.log("reset !!!!!!!!!!!!!!!")
+    this.setState({ ...initialState
+    });
   }
 
   toggleRadio() {
-   this.setState({
-     sleepRadio: !this.state.sleepRadio,
-   });
+    this.setState({
+      sleepRadio: !this.state.sleepRadio,
+    });
   }
 
-  startCalibration() {
+  inputNumberOfBreach = (text) => {
+    this.setState({numberOfBreath: text});
+  }
+
+  onSave = () => {
+
+    console.log("submit!!!:", this.state.numberOfBreath);
+    this.resetSeconds();
+  }
+
+  onCancel = () => {
+
+    console.log("Cancel!!!:", this.state.numberOfBreath);
+    this.resetSeconds();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  startCalibration = () => {
     if (this.state.buttonStatus == BUTTON_STATUS_INIT) {
       this.setState({
         buttonStatus: BUTTON_STATUS_RUNNING,
       });
       //start timer;
+      this.timerID = setInterval(()=>{
+
+        console.log("timer !!!!");
+
+        const newSeconds = this.state.seconds-1;
+        this.setState({
+          seconds: newSeconds,
+        });
+        if (newSeconds == 0){
+          this.setState({
+            buttonStatus: BUTTON_STATUS_END,
+          });
+
+          //TODO make some vibration on phone for seconds
+
+          //stop timer
+          clearInterval(this.timerID);
+        }
+      }, 1000);
     } else if (this.state.buttonStatus == BUTTON_STATUS_RUNNING) {
 
+
+      this.resetSeconds();
+
+      //stop timer
+      clearInterval(this.timerID);
     }
   }
 
@@ -69,21 +141,61 @@ class Record extends React.Component {
 
     console.log("new record props:", this.props.navigation.state);
     console.log("current cat:", this.props.currentCat);
+
+    let inputUI = null;
+
+    if (this.state.buttonStatus == BUTTON_STATUS_END) {
+      // show input text
+      inputUI = (
+        // <Container>
+        <Card>
+          <CardItem>
+            {/* <Form> */}
+                <Item>
+                    {/* <Label>Username</Label> */}
+                    <Input placeholder="Input Number of breath"
+                      onChangeText={this.inputNumberOfBreach}
+                    />
+                </Item>
+            {/* </Form> */}
+              </CardItem>
+            {/* <Card> */}
+              <CardItem>
+                <Button onPress={this.onCancel}>
+                    {/* <Icon active name="thumbs-up" /> */}
+                    <Text>Cancel</Text>
+                </Button>
+                <Right>
+                  <Button onPress={this.onSave}>
+                      {/* <Icon active name="chatbubbles" /> */}
+                      <Text>Save</Text>
+                  </Button>
+                </Right>
+              </CardItem>
+            {/* </Card> */}
+          </Card>
+      );
+    }
+
     return (
       <Container>
         <Content>
-                <ListItem onPress={() => this.toggleRadio()}>
-                    <Radio selected={this.state.sleepRadio} onPress={() => this.toggleRadio()}/>
-                    <Text>睡著時</Text>
-                </ListItem>
-                <ListItem onPress={() => this.toggleRadio()}>
-                    <Radio selected={!this.state.sleepRadio} onPress={() => this.toggleRadio()}/>
-                    <Text>呼吸時</Text>
-                </ListItem>
-                <Button onPress={this.startCalibration}>
-                  <Text>{this.state.buttonStatus}</Text>
-                </Button>
+          <ListItem onPress={() => this.toggleRadio()}>
+              <Radio selected={this.state.sleepRadio} onPress={() => this.toggleRadio()}/>
+              <Text>睡著時</Text>
+          </ListItem>
+          <ListItem onPress={() => this.toggleRadio()}>
+              <Radio selected={!this.state.sleepRadio} onPress={() => this.toggleRadio()}/>
+              <Text>呼吸時</Text>
+          </ListItem>
+          <Button onPress={this.startCalibration}>
+            <Text>{this.state.buttonStatus}</Text>
+          </Button>
+          <Text>
+            {this.state.seconds}
+          </Text>
         </Content>
+        {inputUI}
       </Container>
     );
   }
