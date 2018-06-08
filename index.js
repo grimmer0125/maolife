d2873e8b655ac1e66fddf6e854b3fef54f0231f9
Grimmer <grimmer0125@gmail.com>
2018-06-08 23:14:13 +0800

    Hide more react-native warning which may be fixed in the next few react-native release

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { AppRegistry, YellowBox } from 'react-native';
 import App from './app/App';
 
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated']);
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader requires']);
 
 AppRegistry.registerComponent('maolife', () => App);
