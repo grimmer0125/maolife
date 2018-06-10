4a4ff091236a44cf4c00e4cf5da346e000d325d5
Grimmer <grimmer0125@gmail.com>
2018-06-11 00:30:35 +0800

    Add react-native-mail package

--- android/app/src/main/java/com/maolife/MainApplication.java
+++ android/app/src/main/java/com/maolife/MainApplication.java
@@ -3,6 +3,7 @@ package com.maolife;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.chirag.RNMail.RNMail;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
@@ -38,6 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNMail(),
             new RNI18nPackage(),
             new SvgPackage(),
             new FBSDKPackage(mCallbackManager),
